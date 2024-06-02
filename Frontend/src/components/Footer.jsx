883c61ce54d3d24aca404978d221da85f4ba894c
pedrumond <pe.drumond97@gmail.com>
2024-06-02 16:15:31 -0300

    refactor: update footer style

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -12,29 +12,22 @@ function Footer() {
           />
           <p>Sobre</p>
         </div>
-        <div className='footer__column'>
-          <img
-            src='./assets/recycle.svg'
-            alt='icone de reciclagem'
-            className='footer__icon'
-          />
-          <p>Torne-se um parceiro</p>
-        </div>
       </div>
 
       <div className='footer__column-social '>
         <span>Contato</span>
         <div className='footer__social'>
           <img src='./assets/mail.svg' alt='mail icon' />
-          <p>grupo3ht4@gmail.com</p>{' '}
+          <a
+            className='footer__mail'
+            href='mailto:grupo3ht4@gmail.com?subject="Suporte"'
+          >
+            grupo3ht4@gmail.com
+          </a>{' '}
         </div>
         <div className='footer__social'>
           <img src='./assets/phone.svg' alt='phone icon' />
           <p>(00) 00000-0000</p>{' '}
-        </div>
-        <div className='footer__social'>
-          <img src='./assets/chat.svg' alt='chat icon' />
-          <p>Chat</p>{' '}
         </div>
       </div>
     </footer>
