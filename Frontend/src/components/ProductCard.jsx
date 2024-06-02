0817e3e2767206766b7a2133b9343aa3e3bc081c
altomizawa <alyssontomizawa@hotmail.com>
2024-06-02 20:51:33 -0300

    feat: add product card

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -1,8 +1,46 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './ProductCard.css';
 
-function ProductCard() {
+function ProductCard({item, addItemToCart}) {
+  const [itemAdded, setItemAdded] = useState(false)
+
+  const navigate = useNavigate();
+
+  // ADD VISUAL FEEDBACK WHEN USER ADDS ITEM TO CART
+  const onAddingItem = () => {
+    setItemAdded(true)
+    addItemToCart(item)
+    setTimeout(()=>{setItemAdded(false)},1000)
+  }
+
   return (
-    <div>
-      
+    <div className='featured__card'>
+      {itemAdded && <div style={{position: 'fixed', height: '100%', width: '100%', top: 0, right: 0, backgroundColor: 'rgba(255,255,255,0.9)', borderRadius: '12px'}}>
+        <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <p style={{fontSize: '1rem'}}>Item added to cart!</p>
+        </div>
+      </div>}
+      <img src={item.cover} alt={item.name} onClick={()=>{
+        console.log('clicked')
+        navigate(`/product/${item._id}`)}}/>
+      <div className='featured__container'>
+        <div className='featured__description-wrapper'>
+          <p className='featured__text'>{item.name}</p>
+          <p className='featured__price'>R${item.price}</p>
+        </div>
+
+        <button
+          className='featured__button'
+          onClick={onAddingItem}
+          >
+          <img
+            src='/assets/addToCart.svg'
+            alt='add to cart icon'
+            className='featured__icon'
+            />
+        </button>
+      </div>
     </div>
   )
 }
