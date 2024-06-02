041149b31ebe6ebedf38102846b4646a4ccc799c
altomizawa <alyssontomizawa@hotmail.com>
2024-06-02 08:47:16 -0300

    refactor: create ProductCard component to clean code

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -1,8 +1,27 @@
+import './ProductCard.css';
 
-function ProductCard() {
+function ProductCard({item, addItemToCart}) {
   return (
-    <div>
-      
+    <div className='featured__card'>
+      <img src={item.cover} alt={item.name} />
+      <div className='featured__container'>
+        <div>
+          <p className='featured__text'>{item.name}</p>
+          <p>R${item.price}</p>
+        </div>
+
+        <button
+          className='featured__button'
+          onClick={() => addItemToCart(item)}
+          >
+          {' '}
+          <img
+            src='/assets/shopping_bag.svg'
+            alt='shopping bag icon'
+            className='featured__icon'
+            />
+        </button>
+      </div>
     </div>
   )
 }
