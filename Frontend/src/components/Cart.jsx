942109adf34367cdfe722b52cdd1bbc39df48fed
pedrumond <pe.drumond97@gmail.com>
2024-06-03 04:57:28 -0300

    refactor: update cart styles

--- Frontend/src/components/Cart.jsx
+++ Frontend/src/components/Cart.jsx
@@ -16,55 +16,69 @@ function Cart({
 
   const checkout = () => {
     if (cartItems.length === 0) {
-      return
+      return;
     }
     toggleCart();
-    navigate('/checkout')
-  }
+    navigate('/checkout');
+  };
   return (
     <div className={`cart ${!isCartOpen ? 'cart__opened' : ''}`}>
       <div className='cart__container'>
-        <h2>Carrinho</h2>
         <button className='cart__close-button' onClick={toggleCart}>
-          <img src='./assets/close.svg' alt='close icon' />
+          <img src='/assets/close.svg' alt='close icon' />
         </button>
-        <ul style={{width: '90%', overflowY:"auto"}}>
+        <h2>Carrinho</h2>
+        <ul className='cart__list'>
           {cartItems.map((item) => (
-            <li key={item._id} style={{width: '100%', display: 'grid', gridTemplateColumns: '1fr 4fr 1fr 1fr 1fr'}}>
+            <li key={item._id} className='cart__item'>
               <img src={item.cover} alt={item.name} className='cart__image' />
-                <p style={{textAlign: 'left'}}>{item.name}</p>
-                <p>{item.quantity}</p>
-                <p>R$ {item.price}</p>
+              <div className='cart__text-content'>
+                <p className='cart__name'>{item.name}</p>
+                <p>{item.quantity} un</p>
+              </div>
+              <p className='cart__price'>R$ {item.price}</p>
               <button onClick={() => removeItemFromCart(item._id)}>
                 <img src='/assets/trashIcon.svg' alt='trash icon' />
               </button>
             </li>
           ))}
-          <div style={{textAlign: 'right', marginRight:'1rem', marginTop: '1rem', fontWeight: 'bold'}}>
-            Total:R$
-            {cartItems
-              .reduce((total, item) => total + item.price * item.quantity, 0)
-              .toFixed(2)}
-          </div>
         </ul>
-        <div className='cart__discount'>
-          <input
-            type='text'
-            value={discountCode}
-            onChange={(e) => setDiscountCode(e.target.value)}
-            placeholder='Código de desconto'
-          />
-          <button
-            onClick={handleApplyDiscount}
-            className='cart__discount-button'
-          >
-            Aplicar Desconto
-          </button>
-          <p>{discountMessage}</p>
+        <div className='cart__payment'>
+          <div className='cart__discount'>
+            <input
+              className='cart__input'
+              type='text'
+              value={discountCode}
+              onChange={(e) => setDiscountCode(e.target.value)}
+              placeholder='Código de desconto'
+            />
+            <button
+              onClick={handleApplyDiscount}
+              className='cart__discount-button'
+            >
+              Aplicar Desconto
+            </button>
+            <p>{discountMessage}</p>
+          </div>
+          <div className='card__checkout-content'>
+            <div>
+              Total:R$
+              {cartItems
+                .reduce((total, item) => total + item.price * item.quantity, 0)
+                .toFixed(2)}
+            </div>
+            <button
+              className={
+                cartItems.length === 0
+                  ? 'cart__checkout cart__checkout_inactive'
+                  : 'cart__checkout'
+              }
+              onClick={checkout}
+            >
+              FINALIZAR COMPRA
+            </button>
+          </div>
         </div>
-        <button className={cartItems.length===0 ? 'cart__checkout cart__checkout_inactive' : 'cart__checkout'} onClick={checkout}>
-          FINALIZAR COMPRA
-        </button>
       </div>
     </div>
   );
